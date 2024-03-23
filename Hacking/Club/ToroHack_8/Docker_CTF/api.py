5eefb2e3be7b07b92fb3f4749300c895e5c602a2
lilzims <62032517+lilzims@users.noreply.github.com>
2024-03-22 20:21:09 -0700

    Update and rename CTF_Api.py to api.py

--- Hacking/Club/ToroHack_8/Docker_CTF/api.py
+++ Hacking/Club/ToroHack_8/Docker_CTF/api.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 # Dummy token storage
 tokens = set()
 
-@app.route('/get_token', methods=['GET'])
+@app.route('/get_comment', methods=['GET'])
 def get_token():
     token = secrets.token_hex(16)  # Generate a random token
     tokens.add(token)              # Store the token
-    return jsonify({'token': token})
+    return jsonify({'token': token, 'message': 'This token you have to send, as a post request, to another api enpoint with Content-Type: Application/Json'})
 
-@app.route('/validate_token', methods=['POST'])
+@app.route('/get_the_content', methods=['POST'])
 def validate_token():
     token = request.json.get('token')
     if token in tokens:
-        return jsonify({'valid': True, 'message': 'The flag is ctf(********)'})
+        return jsonify({'valid': True, 'message': 'The flag is ctf(ap1_fl4g)'})
     else:
         return jsonify({'valid': False, 'message': 'Token is invalid'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
